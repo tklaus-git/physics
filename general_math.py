a9c461871482bc673bc438678e992e9b31ab4b33
tklaus-git <tkmail360@gmail.com>
2022-03-23 19:28:02 -0400

    Initial test

--- general_math.py
+++ general_math.py
@@ -1,1 +1,20 @@
 
+
+import numpy as np 
+from general_math import *
+
+
+G = 6.67408*(10**(-11))
+
+'''
+
+'''
+class Celestial:
+    def __init__(self, m, point):
+        self.m = m
+        self.p = point
+
+
+def gravity(b1, b2):
+    return (b1.m * b2.m*G)/((distance(b1.p, b2.p))**2)
+
